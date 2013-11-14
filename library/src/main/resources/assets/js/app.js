480a33ff5462db67bee9fc32e60baa33de594c51
Mahboob Shah Mohammed <mahboobshah16@gmail.com>
2013-11-14 12:42:24 -0800

    Added Fix for page reload issue. Previous code working only in Firefox/Ubuntu

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -12,3 +12,16 @@ $(":button").click(function() {
 		});
 	$(btnisbn).attr("disabled",true);
 });
+
+/*adding a fix to page reload issue*/
+$(document).ready(function() {
+        var books = document.getElementById("books");
+        for (var i = 0, row; row = books.rows[i]; i++) {
+                var bookStatus=books.rows[i].cells[4].innerHTML;
+                if (bookStatus=="lost")
+                {
+                        var newIsbn=books.rows[i].cells[0].innerHTML;
+                        document.getElementById(newIsbn).disabled = true;
+                }
+        }
+}); 
